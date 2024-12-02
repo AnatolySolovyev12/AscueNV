2af6f6aa8d659d775884f92cb0e451bb40f5278d
Анатолий Соловьев <bakalavr12@mail.ru>
2024-12-02 16:57:27 +0500

    Test new lib

--- AscueNV/main.cpp
+++ AscueNV/main.cpp
@@ -7,24 +7,30 @@
 #include <QDebug>
 #include <QJsonArray>
 
+
+#include <tgbot/tgbot.h>
+
+/*
 class TelegramBot : public QObject {
     Q_OBJECT
 
 public:
-    TelegramBot(QObject* parent = nullptr)
-        : QObject(parent)
-    {
+    TelegramBot(const QString& token, QObject* parent = nullptr)
+        : QObject(parent), m_token(token) {
         m_manager = new QNetworkAccessManager(this);
         connect(m_manager, &QNetworkAccessManager::finished, this, &TelegramBot::onResult);
         getUpdates();
+
     }
 
 private:
-    QString m_token = "7880555988:AAHhHkQUARdmJXUT8RB7mrXIgVTQIAkN3RM"; // Замените на ваш токен
+    QString m_token;
     QNetworkAccessManager* m_manager;
 
+
+    QString URLQuery = "https://api.telegram.org/7880555988:AAHhHkQUARdmJXUT8RB7mrXIgVTQIAkN3RM/getMe";
     void getUpdates() {
-        QUrl url(QString("https://api.telegram.org/bot%1/getUpdates").arg(m_token));
+        QUrl url(URLQuery);
         m_manager->get(QNetworkRequest(url));
     }
 
@@ -44,13 +50,47 @@ private slots:
         else {
             qDebug() << "Error:" << reply->errorString();
         }
-       // reply->deleteLater();
+        reply->deleteLater();
     }
 };
 
 int main(int argc, char* argv[]) {
     QCoreApplication a(argc, argv);
-    
-    TelegramBot bot();
+    QString token = "AAHhHkQUARdmJXUT8RB7mrXIgVTQIAkN3RM"; // Замените на ваш токен
+    TelegramBot bot(token);
     return a.exec();
+}
+
+#include "main.moc"
+*/
+
+
+
+#include <stdio.h>
+#include <tgbot/tgbot.h>
+
+int main() {
+    TgBot::Bot bot("7880555988:AAHhHkQUARdmJXUT8RB7mrXIgVTQIAkN3RM");
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi!");
+        });
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+        });
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    }
+    catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+    return 0;
 }
