a53662a990e69478f67e7a2255507446fffcd7ed
Анатолий Соловьев <bakalavr12@mail.ru>
2024-12-02 17:00:53 +0500

    123

--- AscueNV/main.cpp
+++ AscueNV/main.cpp
@@ -69,11 +69,17 @@ int main(int argc, char* argv[]) {
 #include <stdio.h>
 #include <tgbot/tgbot.h>
 
-int main() {
+int main() 
+{
     TgBot::Bot bot("7880555988:AAHhHkQUARdmJXUT8RB7mrXIgVTQIAkN3RM");
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+
+
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) 
+        {
         bot.getApi().sendMessage(message->chat->id, "Hi!");
         });
+
+
     bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
         printf("User wrote %s\n", message->text.c_str());
         if (StringTools::startsWith(message->text, "/start")) {
@@ -81,6 +87,9 @@ int main() {
         }
         bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
         });
+
+
+
     try {
         printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
         TgBot::TgLongPoll longPoll(bot);
@@ -92,5 +101,8 @@ int main() {
     catch (TgBot::TgException& e) {
         printf("error: %s\n", e.what());
     }
+
+
+
     return 0;
 }
