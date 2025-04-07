8c92145a270e1a4f69ca18952018fa25371c60dc
Анатолий Соловьев <bakalavr12@mail.ru>
2025-04-07 15:59:44 +0500

    some correct in some func

--- AscueNV/TelegramJacket.cpp
+++ AscueNV/TelegramJacket.cpp
@@ -24,7 +24,7 @@ TelegramJacket::TelegramJacket(QWidget* parent)
 
 	connect(trayIcon, &QSystemTrayIcon::activated, this, &TelegramJacket::iconActivated);
 
-	bot = new TgBot::Bot("7880555988:AAEPuQ8lPA4wZ8R-YZhkRZiuqC0_ypUzLRo");
+	bot = new TgBot::Bot("");
 
 	messageTest = new TgBot::Message::Ptr();
 
