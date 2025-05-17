ec10144157c4d0bd2f15b9b086f120bfbe72cd35
AnatolySolovyev12 <167902367+AnatolySolovyev12@users.noreply.github.com>
2025-05-17 12:28:35 +0300

    123

--- AscueNV/main.cpp
+++ AscueNV/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char* argv[])
 	QApplication a(argc, argv);
 
 	TelegramJacket tgBotya;
+	//tgBotya.setWindowIcon(QIcon("icon.png"));
+	//tgBotya.setWindowTitle("AscueNV by Solovev");
 
 	return a.exec();
 }
