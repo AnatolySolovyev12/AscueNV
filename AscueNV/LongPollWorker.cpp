d809a6cb0ae48564d19774397bdc01b24b02907f
AnatolySolovyev12 <167902367+AnatolySolovyev12@users.noreply.github.com>
2025-09-05 00:57:31 +0300

    final in today

--- AscueNV/LongPollWorker.cpp
+++ AscueNV/LongPollWorker.cpp
@@ -4,14 +4,14 @@ LongPollWorker::LongPollWorker(TgBot::Bot* bot, QObject* parent)
 	: QObject(parent)
 	, bot_(bot), myTimer(new QTimer)
 {
-	longPoll_ = new TgBot::TgLongPoll(*bot_, 100, 5);
+	longPoll_ = new TgBot::TgLongPoll(*bot_, 100, 15);
 
 	bot_->getEvents().onAnyMessage([this](TgBot::Message::Ptr message) {
 		//  emit messageReceived(message);
 		});
 
 	connect(myTimer, SIGNAL(timeout()), this, SLOT(doLongPoll()));
-	myTimer->setInterval(18000);
+	myTimer->setInterval(15000);
 	myTimer->start();
 }
 
@@ -24,9 +24,9 @@ void LongPollWorker::doLongPoll()
 {
 	try
 	{
-		while (!QThread::currentThread()->isInterruptionRequested()) {   //test////////////////
+		//while (!QThread::currentThread()->isInterruptionRequested()) {   //test////////////////
 			longPoll_->start();  // Блокирующий вызов, работает в своём потоке
-		}
+		//}
 	}
 	catch (TgBot::TgException& e)
 	{
