da73df6fec3c78f5fa643a0c899ecd03398af4be
AnatolySolovyev12 <167902367+AnatolySolovyev12@users.noreply.github.com>
2025-09-14 12:27:39 +0300

    test new realize

--- AscueNV/LongPollWorker.cpp
+++ AscueNV/LongPollWorker.cpp
@@ -1,35 +1,52 @@
 #include "LongPollWorker.h"
 #include <QThread>
 #include <qdebug.h>
+#include <QCoreApplication.h>
 
 LongPollWorker::LongPollWorker(TgBot::Bot* bot, QObject* parent)
     : QObject(parent), bot_(bot)
 {
-    longPoll_ = new TgBot::TgLongPoll(*bot_, 100, 8);
-    
     bot_->getEvents().onAnyMessage([this](TgBot::Message::Ptr message) {
-        emit messageReceived(message);
+        try {
+            emit messageReceived(message);
+        }
+        catch (const std::exception& e) {
+            qWarning() << "Error in message handler:" << e.what();
+        }
         });
-        
+
     printf("Bot username: %s\n\n", bot_->getApi().getMe()->username.c_str());
 }
 
 LongPollWorker::~LongPollWorker()
 {
-    delete longPoll_;
+    // Ничего не удаляем - используем автоматический объект
 }
 
 void LongPollWorker::doLongPoll()
 {
     try
     {
-        // Выполняем цикл, который проверяет флаг прерывания потока
+        TgBot::TgLongPoll longPoll(*bot_, 25, 4);
+        bool inProcessEvents = false; 
+
         while (!QThread::currentThread()->isInterruptionRequested())
         {
-            longPoll_->start();  // блокирующий вызов, ожидает сообщение
+            longPoll.start();
+
+
+            if (!inProcessEvents) {
+                inProcessEvents = true;
+                QCoreApplication::processEvents(QEventLoop::AllEvents, 50);
+                inProcessEvents = false;
+            }
         }
     }
     catch (const TgBot::TgException& e)
+    {
+        emit errorOccurred(QString::fromStdString(e.what()));
+    }
+    catch (const std::exception& e)
     {
         emit errorOccurred(QString::fromStdString(e.what()));
     }
